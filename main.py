06b33e7a15c797610a731a3a3bbea35aed5c30c4
henricosta <pedrocosta.lov@gmail.com>
2023-11-13 19:39:36 -0300

    adiciona funcao para percorrer arquivo e criar dataframes

--- main.py
+++ main.py
@@ -3,85 +3,50 @@ import plotly.express as px
 import json
 import pandas as pd
 
-with open('blocoE.json', 'r') as json_bloco:
-    andares = json.load(json_bloco)
+# Retorna um dicionario com dataframes para cada bloco e andar
+# com o padrão de chave BLOCO_andar.
+def montar_dataframes(nome_arquivo):
+    with open('dados_tratados/' + nome_arquivo) as f:
+        dados = json.load(f)
+        f.close()
 
-media = []
-# 0 -> Térreo, 1 -> Primeiro andar, 2 -> Segundo andar ....
-df_andares = []
+    # 'AB_terreo', 'AB_f1', 'AB_f2', 'C_terreo', 'C_f1', 'C_f2', 'D_terreo', 
+    # 'D_f1', 'D_f2', 'E_terreo', 'E_f1', 'E_f2', 'E_f3', 'E_f4']
+    data_frames = {}
 
-for a in andares:
-    for v in andares[a]:
-        if v['Sinal'] > -60:
-            v['Qualidade'] = 'Boa'
-        elif v['Sinal'] > -79:
-            v['Qualidade'] = 'Média'
-        else:
-            v['Qualidade'] = 'Ruim'
+    for chave_bloco in dados:
+        andar = dados[chave_bloco]
+        for chave_andar in andar:
+            dt = pd.DataFrame(andar[chave_andar])
+            data_frames[f'{chave_bloco}_{chave_andar}'] = dt
 
-    df = pd.DataFrame(andares[a])
-    df_andares.append(df)
-    m = df['Sinal'].mean()
-    media.append(round(m, 2))
+    return data_frames
 
-fig0 = px.bar(df_andares[0], x="Ponto de Acesso", y="Sinal", color='Qualidade', color_discrete_map={
-    'Boa': '#00CC96',
-    'Média': '#FECB52',
-    'Ruim': '#EF553B'
-})
 
-fig1 = px.bar(df_andares[1], x="Ponto de Acesso", y="Sinal", color='Qualidade', color_discrete_map={
-    'Boa': '#00CC96',
-    'Média': '#FECB52',
-    'Ruim': '#EF553B'
-})
+data_frames = montar_dataframes('20_10_noite_tratado.json')
 
-fig2 = px.bar(df_andares[2], x="Ponto de Acesso", y="Sinal", color='Qualidade', color_discrete_map={
-    'Boa': '#00CC96',
-    'Média': '#FECB52',
-    'Ruim': '#EF553B'
-})
+print(data_frames)
 
-fig3 = px.bar(df_andares[3], x="Ponto de Acesso", y="Sinal", color='Qualidade', color_discrete_map={
-    'Boa': '#00CC96',
-    'Média': '#FECB52',
-    'Ruim': '#EF553B'
-})
+# df = pd.DataFrame(dados['AB']['terreo'])
 
-fig4 = px.bar(df_andares[4], x="Ponto de Acesso", y="Sinal", color='Qualidade', color_discrete_map={
-    'Boa': '#00CC96',
-    'Média': '#FECB52',
-    'Ruim': '#EF553B'
-})
+# print(df.count())
 
-app = Dash(__name__)
-app.layout = html.Div(children=[
-    dcc.Graph(
-        id='grafico-pa-bloco-e-t',
-        figure=fig0,
-        style={'width': '500px'}
-    ),
-    dcc.Graph(
-        id='grafico-pa-bloco-e-1',
-        figure=fig1,
-        style={'width': '500px'}
-    ),
-    dcc.Graph(
-        id='grafico-pa-bloco-e-2',
-        figure=fig2,
-        style={'width': '500px'}
-    ),
-    dcc.Graph(
-        id='grafico-pa-bloco-e-3',
-        figure=fig3,
-        style={'width': '500px'}
-    ),
-    dcc.Graph(
-        id='grafico-pa-bloco-e-4',
-        figure=fig4,
-        style={'width': '500px'}
-    ),
-])
+# fig = px.bar()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# # fig0 = px.bar(df_andares[0], x="Ponto de Acesso", y="Sinal", color='Qualidade', color_discrete_map={
+# #     'Boa': '#00CC96',
+# #     'Média': '#FECB52',
+# #     'Ruim': '#EF553B'
+# # })
+
+# app = Dash(__name__)
+# app.layout = html.Div(children=[
+#     dcc.Graph(
+#         id='grafico-pa-bloco-e-t',
+#         figure=fig,
+#         style={'width': '500px'}
+#     ),
+# ])
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
